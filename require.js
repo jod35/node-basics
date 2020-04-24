a05f7c30bba351e492e38f4706e2e2ed5891501c
jona <jona@jona.com>
2020-04-25 00:02:47 +0300

    intro

--- require.js
+++
@@ -1,8 +0,0 @@
-//the require method helps us to access both in built and external modules we wanna use
-
-console.log('Stating app');
-require();
-
-//We can get a complete list of all of the built-in modules in the Node.js API
-docs
-
